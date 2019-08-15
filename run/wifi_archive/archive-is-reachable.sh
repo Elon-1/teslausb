a094645870667d68f5f0d1ba862488afbcedd7df
Daniel Smith <dsmith@ampagency.com>
2019-08-15 00:07:06 -0700

    Test

--- run/wifi_archive/archive-is-reachable.sh
+++ run/wifi_archive/archive-is-reachable.sh
@@ -5,8 +5,8 @@ ap_connections=$(hostapd_cli all_sta | wc -l)
 if [ "$ap_connections" > 1 ]
 then
 	log "At least one wifi client."
-	exit 0
+	exit 1
 	return
 fi
 log "No wifi clients."
-exit 1
+exit 0
